ba7cbd4aee528bd2be8c5e9eb07c48d2cae66b58
llllinyl <2273078123@qq.com>
2026-03-07 22:29:56 +0800

    upload

--- src/qiyana/client.rs
+++ src/qiyana/client.rs
@@ -110,10 +110,10 @@ async fn main() {
     println!("5. Generate query...");
     //let test_string = "cladoniaceae AND cladonia";
     //let test_string = "cladoniaceae OR cladonia";
-    let test_string = "NOT cladoniaceae";
+    //let test_string = "NOT cladoniaceae";
     //let test_string = "(cladoniaceae OR cladonia OR stereocaulaceae) AND podetia AND NOT (swabians OR danube) AND pycnothelia AND stellaris";
     //let test_string = "(cladoniaceae AND cladonia AND stereocaulaceae) AND NOT (swabians OR danube) AND podetia AND NOT banat OR pycnothelia";
-    //let test_string = "(cladoniaceae AND NOT swabians) OR (cladonia AND stereocaulaceae AND NOT danube) OR (podetia AND pycnothelia AND stellaris)";
+    let test_string = "(cladoniaceae AND NOT swabians) OR (cladonia AND stereocaulaceae AND NOT danube) OR (podetia AND pycnothelia AND stellaris)";
     let start_time = Instant::now();
     if mode == 0 {
         let (query, querysum, template, rank_vector) = client.qiyana_query(&test_string);
