30d8250c17689aec3b966ac440e4d5032d3542b6
llllinyl <43959099+llllinyl@users.noreply.github.com>
2026-09-09 16:29:44 +0800

    Update bloomfilter.rs

--- src/utils/bloomfilter.rs
+++ src/utils/bloomfilter.rs
@@ -6,7 +6,7 @@ use std::{
 };
 
 pub const DEFAULT_SIZE: u16 = 2048;
-pub const DEFAULT_HASH_LOOP: u16 = 26;
+pub const DEFAULT_HASH_LOOP: u16 = 28;
 
 pub struct BloomFilter {
     pub size: Cell<u16>,
